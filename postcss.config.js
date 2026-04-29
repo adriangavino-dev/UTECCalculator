4f611f82e38a12b5d964ebbea8efa157e0279c0c
adriangavino-dev <adrian.gavino@utec.edu.pe>
2026-04-28 23:10:41 -0500

    arreglando conflictos

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 export default {
   plugins: {
     '@tailwindcss/postcss': {},
     autoprefixer: {},
   },
-=======
-export default {
-  plugins: {
-    '@tailwindcss/postcss': {},
-    autoprefixer: {},
-  },
->>>>>>> 0b29bea426e57a78137196ebb15dbac6e4fe04b8
 }
